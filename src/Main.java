be4e36c7fdb12bc2abb8e00ded25f85c31edb2e5
Rita Nguema <marvshcherbakova@gmail.com>
2024-11-11 09:12:04 +0300

    Сycle Tasks

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    //точка входа в программу
+        public static void main(String[] args) {
+        System.out.println("===== Task 1: Четные числа от 1 до 20 =====");
+        CycleTasks.Task1();
+        System.out.println("\n===== Task 2: Сумма чисел от 1 до 100 =====");
+        CycleTasks.Task2();
+        System.out.println("\n===== Task 3: Таблица умножения для числа 5 =====");
+        CycleTasks.Task3();
+        System.out.println("\n===== Task 4:Вывод целых чисел массива =====");
+        CycleTasks.Task4();
+        System.out.println("\n===== Task 5: Найти определенную букву =====");
+        CycleTasks.Task5();
+    }
+}
